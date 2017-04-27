07823786b05888d7593e61a221aca440757c327b
hobotoe <kevinsimoes41@gmail.com>
2017-04-27 23:54:09 +1000

    Update Box.java

--- ProjectFiles/Box.java
+++ ProjectFiles/Box.java
@@ -1,4 +1,28 @@
 
-public class Box {
+public class Box implements Asset {
+    private Box box;
+    private Position pos;
 
+
+    public Box() {
+        box = new Box();
+    }
+    @Override
+    public Position getPosition() {
+        return box.pos;
+    }
+
+    @Override
+    public void setPosition(Position p) {
+        box.pos = p;
+    }
+
+    @Override
+    public void move(String direction) {
+        box.pos.changeCoordinate(direction);
+    }
+
+    public boolean atGoal(Position goal) {
+        return box.pos.compareCoordinate(box.pos,goal);
+    }
 }
