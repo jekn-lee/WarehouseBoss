8f307f253430a7a63c2254da3ba34e735500b0b0
hobotoe <kevinsimoes41@gmail.com>
2017-04-27 23:54:32 +1000

    Update Player.java

--- ProjectFiles/Player.java
+++ ProjectFiles/Player.java
@@ -1,4 +1,24 @@
+public class Player implements Asset  {
+    private Player player;
+    private Position pos;
 
-public class Player {
 
+    public Player() {
+        player = new Player();
+    }
+
+    @Override
+    public Position getPosition() {
+        return player.pos;
+    }
+
+    @Override
+    public void setPosition(Position p) {
+        player.pos = p;
+    }
+
+    @Override
+    public void move(String direction) {
+        player.pos.changeCoordinate(direction);
+    }
 }
