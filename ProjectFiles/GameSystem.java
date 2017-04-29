d1e99032a51f8ff4d9e65c4747b81724449322ec
alax802 <alex.la8020@gmail.com>
2017-04-29 13:23:42 +1000

    Update GameSystem.java

--- ProjectFiles/GameSystem.java
+++ ProjectFiles/GameSystem.java
@@ -3,7 +3,51 @@ public class GameSystem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int nBox = 2; // predefined number of boxes/goals, need to extend where you specify this and generate a random map
+		Map m = new Map(); //current map constructor makes predefined map
+		Player player = new Player();
+		ArrayList<Goal> goals = new ArrayList<Goal>;
+		ArrayList<Box> boxes = new ArrayList<Box>;
+		goals.add(new Goal(3,2)); // TODO
+		goals.add(new Goal(4,2)); // TODO
+		boxes.add(new Box(3,4)); // TODO
+		boxes.add(new Box(4,4)); // TODO
+		
+		Position playerPos = player.getPosition();
+		String direction = "Right";
+		int move = m.checkMove(direction);
+		if (move == NO_MOVE) { //update nothing, remove later
+		} else if (move == MOVE_PLAYER) { //update player position
+			player.move(direction);
+		} else if (move == MOVE_PLAYER_BOX) { // update player position and box position
+			for (int i = 0; i < boxes.size(); i++) { // find box whose current position is in the direction of where the player wants to move
+				Box cur = boxes.get(i);
+				if (cur.getPosition.comparePosition(getAdjacentCoordinate(playerPos, direction)))
+					break;
+			}
+			player.move(direction);
+			cur.move(direction);
+			// TODO: checking if box has been moved to a goal
+		}
 	}
 
+	private Position getAdjacentCoordinate(Position pos, String direction) {
+		int x = pos.getX();
+		int y = pos.getY();
+		switch(direction) {
+		case "Left":
+			x--;
+			break;
+		case "Right":
+			x++;
+			break;
+		case "Up":
+			y--;
+			break;
+		case "Down":
+			y++;
+			break;
+		}
+		return new Position(x,y);
+	}
 }
