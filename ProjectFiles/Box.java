df2deefa674518db3c8ab10f24563febdb85c10a
hobotoe <kevinsimoes41@gmail.com>
2017-05-01 19:06:59 +1000

    Update Box.java

--- ProjectFiles/Box.java
+++ ProjectFiles/Box.java
@@ -1,10 +1,12 @@
 
 public class Box implements Asset {
     private Position pos;
+    private boolean isAtGoal;
 
 
-    public Box(int x, int y) {
+    public Box(int x, int y, boolean g) {
         this.pos = new Position(x,y);
+        this.isAtGoal = g;
     }
     @Override
     public Position getPosition() {
@@ -23,5 +25,13 @@ public class Box implements Asset {
 
     public boolean atGoal(Position goal) {
         return this.pos.compareCoordinate(this.pos,goal);
+    }
+
+    public boolean getGoalState() {
+        return this.isAtGoal;
+    }
+
+    public void setGoalSate(boolean g) {
+        this.isAtGoal = g;
     }
 }
