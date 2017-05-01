e315f03fbcfb856a6c9a9763d7383f0dfe260add
alax802 <alex.la8020@gmail.com>
2017-05-01 19:54:59 +1000

    Update GameSystem.java

--- ProjectFiles/GameSystem.java
+++ ProjectFiles/GameSystem.java
@@ -12,27 +12,63 @@ public class GameSystem {
 		Player player = new Player();
 		ArrayList<Goal> goals = new ArrayList<Goal>;
 		ArrayList<Box> boxes = new ArrayList<Box>;
-		goals.add(new Goal(3,2)); // TODO
-		goals.add(new Goal(4,2)); // TODO
-		boxes.add(new Box(3,4)); // TODO
-		boxes.add(new Box(4,4)); // TODO
+		goals.add(new Goal(3,2,false)); // TODO
+		goals.add(new Goal(4,2,false)); // TODO
+		boxes.add(new Box(3,4,false)); // TODO
+		boxes.add(new Box(4,4,false)); // TODO
 		
+		Scanner sc = null;
+		try {
+			sc = new Scanner(new FileReader(args[0]));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while (sc.hasNextLine()) {
+			String direction = sc.nextLine();
+			makeMove(direction, player, m, boxes, goals);
+			if (!checkGameState) break;
+		}
+		System.out.println("YOU WIN!!!");
+	}
+
+	void makeMove(String direction, Player player, Map m, ArrayList<Box> boxes, ArrayList<Goal> goals) {	
 		Position playerPos = player.getPosition();
-		String direction = "Right";
 		int move = m.checkMove(direction);
 		if (move == NO_MOVE) { //update nothing, remove later
 		} else if (move == MOVE_PLAYER) { //update player position
 			player.move(direction);
 		} else if (move == MOVE_PLAYER_BOX) { // update player position and box position
+			Position newPlayerPos = getAdjacentCoordinate(playerPos, direction);
 			for (int i = 0; i < boxes.size(); i++) { // find box whose current position is in the direction of where the player wants to move
-				Box cur = boxes.get(i);
-				if (cur.getPosition.comparePosition(getAdjacentCoordinate(playerPos, direction))) // found the box
+				Box curBox = boxes.get(i);
+				Position originalBoxPos = curBox.getPosition();
+				if (originalBoxPos.comparePosition(newPlayerPos)) {// found the box
+					curBox.move(direction);
 					break;
+				}
 			}
 			player.move(direction);
-			cur.move(direction);
-			// TODO: checking if box has been moved to a goal
+			for (int i = 0; i < goals.size(); i++) {
+				Goal curGoal = goals.get(i);
+				if (goals.getPosition.comparePosition(curBox.getPosition())) { // moved box into goal
+					curGoal.setSatisfied(true);
+				//} else if (goals.getPosition.comparePosition(originalBoxPos)) { // moved box out of goal
+				//	curGoal.setSatisfied(false);
+				//}
+			}
 		}
+	}
+
+	boolean checkGameState(ArrayList<Goal> goals) {
+		boolean finished = true;
+		for (i = 0 i < goals.size(); i++) {
+			if (goals.get(i).getSatisfied() == false) {
+				finished = false;
+				break;
+			}
+		}
+		return finished;
 	}
 
 	private Position getAdjacentCoordinate(Position pos, String direction) {
