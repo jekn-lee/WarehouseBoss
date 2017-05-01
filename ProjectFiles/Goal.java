1a03cd76cd9c85a8b296cd749364085b240c4665
hobotoe <kevinsimoes41@gmail.com>
2017-05-01 18:50:11 +1000

    Update Goal.java

--- ProjectFiles/Goal.java
+++ ProjectFiles/Goal.java
@@ -1,4 +1,39 @@
+package com.example;
 
-public class Goal {
+/**
+ * Created by kevin on 5/1/2017.
+ */
+
+public class Goal implements Asset {
+    private Position pos;
+    private boolean isSatisfied;
+
+    public Goal(int x, int y, boolean b) {
+        this.pos = new Position(x, y);
+        this.isSatisfied = b;
+    }
+    @Override
+    public Position getPosition() {
+        return this.pos;
+    }
+
+    @Override
+    public void setPosition(Position p) {
+        this.pos = p;
+
+    }
+
+    @Override
+    public void move(String direction) {
+
+    }
+
+    public boolean getSatisfied() {
+        return this.isSatisfied;
+    }
+
+    public void setSatisfied(boolean b) {
+        this.isSatisfied = b;
+    }
 
 }
